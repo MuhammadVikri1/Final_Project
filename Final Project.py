122cee13d7a7df85318648c7a467a0abec6ec91a
MuhammadVikri1 <muhammadvikri000@gmail.com>
2021-04-02 08:33:44 +0700

    Tambah Fitur input

--- Final Project.py
+++ Final Project.py
@@ -1,8 +1,8 @@
 import smtplib
 from email.message import EmailMessage
-Pengirim_Email = "ahmadalbedahani@gmail.com"
+Pengirim_Email = input('Apa alamat email-mu? : ')
 Penerima_Email = open("List_Penerima.txt")
-Password = input('Apa password Emailmu?:) : ')
+Password = input('Silahkan tulis password Emailmu:) : ')
 PesanBaru = EmailMessage()    #membuat objek pada kelas pesan email
 PesanBaru['Subject'] = "Undangan Reuni Kampus 2021" #Definisi Subjek Email
 PesanBaru['From'] = Pengirim_Email  #Definisi Pengirim Email
@@ -11,10 +11,15 @@ PesanBaru.set_content('Assalamualaikum ... Apa kabar semua? Mari datang reunian 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     
     smtp.login(Pengirim_Email, Password) #Masuk ke SMTP server
-    smtp.send_message(PesanBaru)      #Kirim Email dengan send_message method dengan passing EmailMessage object
+    smtp.send_message(PesanBaru)      #Kirim Email dengan send_message method dengan cara passing EmailMessage object
+
+
+
+
+
+
+
+
 
 
 #Sumber : https://ichi.pro/id/cara-mengirim-email-menggunakan-python-dengan-lampiran-file-skrip-215012666985067
-
-
-# Email_Penerima = open("Email_Penerima.txt "r+")
